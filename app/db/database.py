7261fa6e8d799de52db05917390885edeee858ef
Batuhan Mert Elma <batuhanmertelma@Batuhan-MacBook-Air.local>
2026-08-06 22:46:51 +0200

    adding docker

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgres@localhost:5433/expense_tracker"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres@localhost:5433/expense_tracker")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
